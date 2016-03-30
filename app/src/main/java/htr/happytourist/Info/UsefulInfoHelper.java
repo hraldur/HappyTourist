6a25391e7546e0c131b5be2025b01e72d33fb59e
hlin89 <hlingu89@gmail.com>
2016-03-30 20:40:36 +0000

    test med database fyrir useful info

---
+++ app/src/main/java/htr/happytourist/Info/UsefulInfoHelper.java
@@ -0,0 +1,32 @@
+package htr.happytourist.Info;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hlingunnlaugsdottir on 30/03/16.
+ */
+public class UsefulInfoHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "usefulinfo.db";
+    
+    public UsefulInfoHelper(Context context){
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + PhrasesDbSchema.PhrasesTable.NAME + "(" + "_id integer primary key autoincrement, " +
+                PhrasesDbSchema.PhrasesTable.Cols.ISL + "," +
+                PhrasesDbSchema.PhrasesTable.Cols.ENG + "," + ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
